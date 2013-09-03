8a6f06fb32764652fd08c09043d5efb2908da924
Lin Jen-Shin <godfat@godfat.org>
2013-09-04 00:20:56 +0800

    missed a rename

--- lib/request-replay/middleware.rb
+++ lib/request-replay/middleware.rb
@@ -9,7 +9,7 @@ class RequestReplay::Middleware
   def call env
     # We don't want to read the socket in a thread, so create it in main
     # thread, and send the data in a thread as we don't care the responses
-    Thread.new(ReplayRequest.new(env, @host, @headers), &:start)
+    Thread.new(RequestReplay.new(env, @host, @headers), &:start)
     @app.call(env)
   end
 end
