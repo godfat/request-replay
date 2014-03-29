6541e8651c80288c93e0e16e0d1ca843bb2487a5
Lin Jen-Shin <godfat@godfat.org>
2014-03-30 02:37:41 +0800

    introduce rewrite_env to make the code more clean

--- lib/request-replay/middleware.rb
+++ lib/request-replay/middleware.rb
@@ -7,17 +7,19 @@ class RequestReplay::Middleware
   end
 
   def call env
-    # Unfortunately, we need to dup env because other middleware might be
-    # modifying it and make RequestReplay not able to get the original env.
-    rr_env = if rewrite = @options[:rewrite_env]
-               rewrite.call(env.dup)
-             else
-              env.dup
-             end
-
     # We don't want to read the socket in a thread, so create it in main
     # thread, and send the data in a thread as we don't care the responses.
-    Thread.new(RequestReplay.new(rr_env, @host, @options), &:start)
+    Thread.new(RequestReplay.new(rewrite_env(env), @host, @options), &:start)
     @app.call(env)
+  end
+
+  def rewrite_env env
+    # Unfortunately, we need to dup env because other middleware might be
+    # modifying it and make RequestReplay not able to get the original env.
+    if rewrite = @options[:rewrite_env]
+      rewrite.call(env.dup)
+    else
+      env.dup
+    end
   end
 end
